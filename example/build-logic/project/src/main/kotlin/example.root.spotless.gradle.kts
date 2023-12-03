8d98bfc7787dc96b146c4e5b1d95f60e5f3261d6
Omico <me@omico.icu>
2023-12-03 03:47:33 -0800

    Update dependencies

--- example/build-logic/project/src/main/kotlin/example.root.spotless.gradle.kts
+++ example/build-logic/project/src/main/kotlin/example.root.spotless.gradle.kts
@@ -14,9 +14,6 @@ consensus {
         kotlin(
             targets = setOf("**/src/*/kotlin/**/*.kt"),
             editorConfigOverride = ConsensusSpotlessTokens.Kotlin.editorConfigOverride + mapOf(
-                // TODO waiting for ktlint 1.0.1
-                // "ktlint_function_naming_ignore_when_annotated_with" to "Composable",
-                "ktlint_standard_function-naming" to "disabled",
                 "ktlint_standard_property-naming" to "disabled",
             ),
         )
