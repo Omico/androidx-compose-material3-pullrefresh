eb34190262033a07adcd5d6e625d8386153aa757
Omico <me@omico.icu>
2024-01-30 19:58:03 -0800

    Update to Consensus 0.8.0

--- example/build-logic/project/src/main/kotlin/example.root.git.gradle.kts
+++ example/build-logic/project/src/main/kotlin/example.root.git.gradle.kts
@@ -1,4 +1,3 @@
-import me.omico.consensus.dsl.isGitDirectoryExists
 import me.omico.consensus.dsl.requireRootProject
 
 plugins {
@@ -9,7 +8,6 @@ requireRootProject()
 
 consensus {
     git {
-        if (!isGitDirectoryExists) return@git
         hooks {
             preCommit {
                 appendLine("#!/bin/sh")
