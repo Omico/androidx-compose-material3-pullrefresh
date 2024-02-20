1032f21b42e5df9273f954633902f60959c98fb7
Omico <me@omico.icu>
2024-02-20 05:06:46 -0800

    Update the description in local.properties.template

--- example/build-logic/project/src/main/kotlin/me/omico/compose/project/internal/LocalPropertiesTemplate.kt
+++ example/build-logic/project/src/main/kotlin/me/omico/compose/project/internal/LocalPropertiesTemplate.kt
@@ -13,17 +13,20 @@ internal fun generateLocalPropertiesTemplate(
     val kotlinVersion = versions.versionFor(ProjectVersions.KOTLIN)
     localPropertiesTemplateFile.writeText(
         """
-            |# Change these to fit your project's needs
-            |# Note $androidxComposeBomVersion contains androidx.compose.material:material:$androidxComposeMaterialVersion
-            |# See https://developer.android.com/jetpack/androidx/releases/compose-material#$androidxComposeMaterialVersion
-            |# See also, https://developer.android.com/jetpack/compose/bom/bom-mapping
-            |# Kotlin $kotlinVersion require compose compiler $androidxComposeCompilerVersion
-            |# See https://developer.android.com/jetpack/androidx/releases/compose-compiler#$androidxComposeCompilerVersion
-            |project.android.gradle.plugin.version=$androidGradlePluginVersion
-            |project.compose.bom.version=$androidxComposeBomVersion
-            |project.compose.compiler.version=$androidxComposeCompilerVersion
-            |project.kotlin.version=$kotlinVersion
-            |
+        |# Change these to fit your project's needs
+        |#
+        |# Compose BOM $androidxComposeBomVersion contains androidx.compose.material:material:$androidxComposeMaterialVersion
+        |# See https://developer.android.com/jetpack/compose/bom/bom-mapping
+        |# See also, https://developer.android.com/jetpack/androidx/releases/compose-material#$androidxComposeMaterialVersion
+        |#
+        |# Always use latest Compose Compiler to gain the latest features and bug fixes
+        |# See https://developer.android.com/jetpack/androidx/releases/compose-compiler#$androidxComposeCompilerVersion
+        |#
+        |project.android.gradle.plugin.version=$androidGradlePluginVersion
+        |project.compose.bom.version=$androidxComposeBomVersion
+        |project.compose.compiler.version=$androidxComposeCompilerVersion
+        |project.kotlin.version=$kotlinVersion
+        |
         """.trimMargin(),
     )
 }
